7f7113955bb8e46fc16a4368d647a8348855ab76
VladPython123 <156468245+VladPython123@users.noreply.github.com>
2024-01-12 21:36:52 +0200

    Create main.py
    Основний файл для запуску бота

---
+++ main.py
@@ -0,0 +1,24 @@
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher
+from aiogram.enums.parse_mode import ParseMode
+from aiogram.fsm.storage.memory import MemoryStorage
+
+import handlers 
+import callback
+import callback2 
+import state
+
+BOT_TOKEN = '6862881309:AAGb2D4bXWM7rKbnU_zzUbDp6BqneJqrDRI'
+
+async def main():
+    bot = Bot(token=BOT_TOKEN, parse_mode=ParseMode.HTML)
+    dp = Dispatcher(storage=MemoryStorage())
+    dp.include_routers(callback.router, callback2.router, handlers.router, state.router)
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    asyncio.run(main())
