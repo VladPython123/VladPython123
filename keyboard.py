4adb5638264b248ca8a3280bb05b76dd88dc5500
VladPython123 <156468245+VladPython123@users.noreply.github.com>
2024-01-12 21:44:50 +0200

    Create keyboard.py
    Кнопки до бота

---
+++ keyboard.py
@@ -0,0 +1,54 @@
+from aiogram import types 
+
+
+
+buttons1 = [
+           [types.InlineKeyboardButton(text="Товари", callback_data="g1"),],
+           [types.InlineKeyboardButton(text="Корзина", callback_data="g2"), ] ,
+           ]
+
+buttons2 = [
+           [types.InlineKeyboardButton(text="iPhone 15 Pro Max", callback_data="gh1"),],
+           [types.InlineKeyboardButton(text="Galaxy S22 Ultra", callback_data="gh2"), ] ,
+           [types.InlineKeyboardButton(text="Redmi Note 13 Pro", callback_data="gh3"),],
+           [types.InlineKeyboardButton(text="POCO X6 Pro 5G", callback_data="gh4"),   ],
+           [types.InlineKeyboardButton(text="HUAWEI Nova 9", callback_data="gh5"),    ],
+           [types.InlineKeyboardButton(text="⬅️Меню", callback_data="wе3"),    ],
+           ]
+
+buttons3 = [
+           [types.InlineKeyboardButton(text="Обнулити заказ ", callback_data="wе1"),],
+           [types.InlineKeyboardButton(text="Заказати", callback_data="wе2"),],
+           [types.InlineKeyboardButton(text="⬅️Меню", callback_data="wе3"), ] ,
+           ]
+
+
+
+
+but1 = [
+           [types.InlineKeyboardButton(text="Добавити до корзини ➕ ", callback_data="iPhone"),],
+           [types.InlineKeyboardButton(text="⬅️Назад", callback_data="w2"), ] ,
+           ]
+but2 = [
+           [types.InlineKeyboardButton(text="Добавити до корзини ➕ ", callback_data="Galaxy"),],
+           [types.InlineKeyboardButton(text="⬅️Назад", callback_data="w2"), ] ,
+           ]
+but3 = [
+           [types.InlineKeyboardButton(text="Добавити до корзини ➕ ", callback_data="Redmi"),],
+           [types.InlineKeyboardButton(text="⬅️Назад", callback_data="w2"), ] ,
+           ]
+but4 = [
+           [types.InlineKeyboardButton(text="Добавити до корзини ➕ ", callback_data="POCO"),],
+           [types.InlineKeyboardButton(text="⬅️Назад", callback_data="w2"), ] ,
+           ]
+but5 = [
+           [types.InlineKeyboardButton(text="Добавити до корзини ➕ ", callback_data="HUAWEI"),],
+           [types.InlineKeyboardButton(text="⬅️Назад", callback_data="w2"), ] ,
+           ]
+
+
+
+buttons4 = [
+           [types.InlineKeyboardButton(text="Повторити", callback_data="wz1"),],
+           [types.InlineKeyboardButton(text="Заказати", callback_data="wz2"),],
+]
